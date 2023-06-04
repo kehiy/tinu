bd59852a58030e050d9316caac605345b303c08d
kehiy <kehiiiiya@gmail.com>
2023-06-04 10:28:15 -0700

    item componnent done

---
+++ view/src/components/Item.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Card from "./Card.svelte";
+</script>
+
+<Card>
+    <p>Tinu: https://localhost:3000/{"40c24a8c6d354d4f"}</p>
+    <p>url: {"url"}</p>
+    <p>Clicked: {"clicked"}</p>
+    <button class="update">Update</button>
+    <button class="delete">Delete</button>
+</Card>
+
+<style>
+    button {
+        color: white;
+        font-weight: bolder;
+        border: none;
+        padding: .75rem;
+        border-radius: 4px;
+    }
+    .update{
+        background-color: yellowgreen;
+    }
+    .delete{
+        background-color: red;
+    }
+</style>
