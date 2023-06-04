014a5d6f86a85b5ab96a19cef39c382d38e227f0
kehiy <kehiiiiya@gmail.com>
2023-06-04 06:38:21 -0700

    id util debug

--- app/utils/generate_id.go
+++ app/utils/generate_id.go
@@ -20,7 +20,7 @@ func getMacAddr() (addr string) {
 	interfaces, err := net.Interfaces()
 	if err == nil {
 		for _, i := range interfaces {
-			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+			if i.Flags&net.FlagUp != 0 && !bytes.Equal(i.HardwareAddr, nil) != 0 {
 				// Don't use random as we have a real address
 				addr = i.HardwareAddr.String()
 				break
