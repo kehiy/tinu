ffa917fd71de378b8fc1557164322ca90531f720
kehiy <kehiiiiya@gmail.com>
2023-06-04 06:35:40 -0700

    data base methods done

---
+++ app/model/tinu.go
@@ -0,0 +1,42 @@
+package model
+
+func GetAll() ([]Tinu, error){
+	var tinus []Tinu
+	// search for all tinus in db
+	tx := db.Find(&tinus)
+	if tx.Error != nil {
+		return []Tinu{}, tx.Error
+	}
+
+	return tinus, nil
+}
+
+func GetOne(id string) (Tinu, error){
+	var tinu Tinu
+	// search tinu with id in db
+	tx := db.Where("id = ?", id).First(&tinu)
+	if tx.Error != nil {
+		return Tinu{}, tx.Error
+	}
+
+	return tinu, nil
+}
+
+func CreateTinu(tinu Tinu) error {
+	// add new tinu to db
+	tx := db.Create(&tinu)
+	return tx.Error
+}
+
+func UpdateTinu(tinu Tinu) error {
+	// save updated tinu to db
+	tx := db.Save(&tinu)
+	return tx.Error
+}
+
+func DeleteTinu(id string) error {
+	tx := db.Unscoped().Delete(&Tinu{}, id)
+	return tx.Error
+}
+
+
