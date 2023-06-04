f431cee920bea3e0604c88dd14d4afbab216f1de
kehiy <kehiiiiya@gmail.com>
2023-06-04 07:15:15 -0700

    server done

---
+++ app/server/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/kehiy/tinu/model"
+)
+
+func getAll(ctx *fiber.Ctx) error {
+	tinus, err := model.GetAll()
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message":"internal server error" + err.Error(),
+		})
+	}
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"data":tinus,
+	})
+}
+
+func SetupAndListen() {
+	router := fiber.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	router.Get("/tinus", getAll)
+
+	router.Listen(":3000")
+}
