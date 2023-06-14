34739196ad00ffa5d4982b3eb1a56e3d49a4c44d
kehiy <kehiiiiya@gmail.com>
2023-06-14 14:28:06 -0500

    use mush id

--- app/go.mod
+++ app/go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kehiy/mushid v1.0.7 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- app/utils/generate_id.go
+++ app/utils/generate_id.go
@@ -1,43 +1,9 @@
 package utils
 
 import (
-	"bytes"
-	"hash/crc32"
-	"fmt"
-	"net"
-	"os"
-	"strconv"
-	"time"
+	"github.com/kehiy/mushid"
 )
 
-func crc32Hash(s string) string {
-	table := crc32.MakeTable(crc32.Castagnoli)
-	hash := crc32.Checksum([]byte(s), table)
-	return fmt.Sprintf("%08x", hash)
-}
-
-func getMacAddr() (addr string) {
-	interfaces, err := net.Interfaces()
-	if err == nil {
-		for _, i := range interfaces {
-			if i.Flags&net.FlagUp != 0 && !bytes.Equal(i.HardwareAddr, nil){
-				// Don't use random as we have a real address
-				addr = i.HardwareAddr.String()
-				break
-			}
-		}
-	}
-	return
-}
-
-// this is the logic of MushId => https://github.com/kehiy/mushid
 func GenerateId() string {
-	Time := strconv.Itoa(int(time.Now().UnixNano()))
-	Pid := os.Getpid()
-	StrPid := strconv.Itoa(Pid)
-	var MAC = getMacAddr()
-
-	var Mush = StrPid + "-" + MAC
-	HashesMush := crc32Hash(Time) + crc32Hash(Mush)
-	return HashesMush
+	return MushId.New()
 }
