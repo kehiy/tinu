5dcb764d6c7b26e89ef5d00ac2a4d31aaebf11bd
kehiy <kehiiiiya@gmail.com>
2023-06-26 13:52:46 -0500

    feat: user model

--- models/model.go
+++ models/model.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -12,9 +13,19 @@ import (
 var db *gorm.DB
 
 type Tinu struct {
-	ID      string `json:"id" gorm:"primaryKey"`
-	URL     string `json:"url" gorm:"not null"`
-	Clicked uint64 `json:"clicked"`
+	ID        string `json:"id" gorm:"primaryKey"`
+	URL       string `json:"url" gorm:"not null"`
+	Clicked   uint64 `json:"clicked"`
+	UserID    string `json:"user_id" gorm:"foreignKey:UserID"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type User struct {
+	ID       string `json:"id" gorm:"primaryKey"`
+	Email    string `json:"email" gorm:"not null;unique_index"`
+	PassWord string `json:"password" gorm:"not null"`
+	Tinus    []Tinu `json:"tinus"`
 }
 
 func loadEnv() (string, string, string, string, string) {
@@ -46,6 +57,11 @@ func Setup() {
 
 	err = db.AutoMigrate(&Tinu{})
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic(err)
 	}
 }
